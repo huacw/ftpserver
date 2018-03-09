cf0e060bbfdd39358cd805f88ddea07f568df14d
华成伍 <chengwu.hua@mtime.com>
2018-03-09 14:12:30 +0800

    格式化

--- src-api/com/sea/ftp/util/CiphertextUtils.java
+++ src-api/com/sea/ftp/util/CiphertextUtils.java
@@ -15,40 +15,37 @@ import com.sea.ftp.message.i18n.LocalizedMessageResource;
  * @author sea
  */
 public class CiphertextUtils {
-	private static LocalizedMessageResource lmr = LocalizedMessageResource.newInstance();
+    private static LocalizedMessageResource lmr = LocalizedMessageResource.newInstance();
 
-	private CiphertextUtils() {
-	}
+    private CiphertextUtils() {}
 
-	/**
-	 * 加密字符串
-	 * 
-	 * @param sourceStr
-	 *            需要加密目标字符串
-	 * @param algorithms
-	 *            算法(如:MD2,MD5,SHA1,SHA256,SHA384,SHA512)
-	 * @return
-	 */
-	public static String passAlgorithmsCiphering(String sourceStr, EncryptedStrategyType algorithms) {
-		switch (algorithms) {
-			case MD2:
-				return DigestUtils.md2Hex(sourceStr);
-			case MD5:
-				return DigestUtils.md5Hex(sourceStr);
-			case SHA1:
-				return DigestUtils.sha1Hex(sourceStr);
-			case SHA256:
-				return DigestUtils.sha256Hex(sourceStr);
-			case SHA384:
-				return DigestUtils.sha384Hex(sourceStr);
-			case SHA512:
-				return DigestUtils.sha512Hex(sourceStr);
-			case none:
-				return sourceStr;
-			default:
-				MessageCode code = MessageCode.newMessageCode(MessageType.Error);
-				code.setMsgKey("illegal.encryption.error");
-				throw new FTPServerRuntimeException(lmr.getMessage(code,algorithms.name()));
-		}
-	}
+    /**
+     * 加密字符串
+     * 
+     * @param sourceStr 需要加密目标字符串
+     * @param algorithms 算法(如:MD2,MD5,SHA1,SHA256,SHA384,SHA512)
+     * @return
+     */
+    public static String passAlgorithmsCiphering(String sourceStr, EncryptedStrategyType algorithms) {
+        switch (algorithms) {
+            case MD2:
+                return DigestUtils.md2Hex(sourceStr);
+            case MD5:
+                return DigestUtils.md5Hex(sourceStr);
+            case SHA1:
+                return DigestUtils.sha1Hex(sourceStr);
+            case SHA256:
+                return DigestUtils.sha256Hex(sourceStr);
+            case SHA384:
+                return DigestUtils.sha384Hex(sourceStr);
+            case SHA512:
+                return DigestUtils.sha512Hex(sourceStr);
+            case none:
+                return sourceStr;
+            default:
+                MessageCode code = MessageCode.newMessageCode(MessageType.Error);
+                code.setMsgKey("illegal.encryption.error");
+                throw new FTPServerRuntimeException(lmr.getMessage(code, algorithms.name()));
+        }
+    }
 }
